172067fef58fab69a6b50767e25965fafc7a3cc4
Omar Abid <omar.abid4@gmail.com>
2020-04-21 14:36:18 -0400

    blah

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="test_repo1_od",
+    version="0.0.1",
+    author="Omar Abid",
+    author_email="omar.abid4@gmail.com",
+    description="A package for math stuff",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/omarabid59/test_repo1/",
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    package_dir={"": "src"},
+    packages=find_namespace_packages(where="src")
+)
---
+++ src/disney_safety/od_another/some_math.py
@@ -0,0 +1,6 @@
+class SomeMath:
+	def __init__(self):
+		pass
+
+	def multiply(x, y):
+		return x*y
